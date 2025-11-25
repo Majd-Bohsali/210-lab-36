8d5a8bf788a6f3dec88a685ee9d570964934c9b7
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-11-24 17:28:14 -0800

    Milestone 2

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -1,11 +1,20 @@
 // COMSC210 | Lab 36 | Majd Bohsali
 // IDE used: Visual Studio Code
 #include <iostream>
-#include "IntBinaryTree.h"
+#include "StringBinaryTree.h"
 using namespace std;
 
 int main() {
-    
+    StringBinaryTree tree; 
+
+    tree.insertNode("A");
+    tree.insertNode("B");
+    tree.insertNode("C");
+    tree.insertNode("D");
+    tree.displayInOrder();
+    cout << endl << endl; 
+    tree.remove("C");
+    tree.displayInOrder();
 
     return 0;
 }
