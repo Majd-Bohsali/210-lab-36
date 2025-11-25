17459181fc15bfbfe1e3dd6d748318c2d496904c
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-11-24 17:34:56 -0800

    Milestone 3

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -2,19 +2,28 @@
 // IDE used: Visual Studio Code
 #include <iostream>
 #include "StringBinaryTree.h"
+#include <string> 
+#include <iostream>
+#include <fstream>
 using namespace std;
 
 int main() {
     StringBinaryTree tree; 
+    string code; 
+    ifstream inputFile("codes.txt"); 
+    
+    if(inputFile.is_open()) { 
+        while(getline(inputFile, code)) { 
+            tree.insertNode(code); 
+        }
+        inputFile.close(); 
+    } else { 
+        cout << "File did not open" << endl; 
+    }
 
-    tree.insertNode("A");
-    tree.insertNode("B");
-    tree.insertNode("C");
-    tree.insertNode("D");
-    tree.displayInOrder();
-    cout << endl << endl; 
-    tree.remove("C");
-    tree.displayInOrder();
+    cout << "Output tree using postorder" << endl; 
+
+    tree.displayPostOrder(); 
 
     return 0;
 }
