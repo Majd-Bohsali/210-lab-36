db31ef5db4e1150dec40d02a9fbba15a0f52c936
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-11-25 18:00:25 -0800

    writting if-else block for menu actions

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -7,11 +7,14 @@
 #include <fstream>
 using namespace std;
 
+void menuSelection(); 
+
 int main() {
     StringBinaryTree tree; 
     string code; 
     ifstream inputFile("codes.txt"); 
-    
+    int selection = 0; 
+
     if(inputFile.is_open()) { 
         while(getline(inputFile, code)) { 
             tree.insertNode(code); 
@@ -21,24 +24,44 @@ int main() {
         cout << "File did not open" << endl; 
     }
 
-    cout << "Output tree using postorder" << endl; 
-
+    cout << "Outputing tree in postorder" << endl; 
     tree.displayPostOrder(); 
+
+    while (selection != 6) { 
+        menuSelection(); 
+        cout << "Selection -> ";
+        cin >> selection; 
+        if(selection == 1) { 
+            string newCode; 
+            cout << "Enter code to add to tree: "; 
+            cin >> newCode; 
+            tree.insertNode(newCode); 
+        } else if (selection == 2) {
+            string delCode; 
+            cout << "Enter a code to delete: "; 
+            cin >> delCode; 
+            tree.remove(delCode); 
+        } else if (selection == 3) { 
+            string searchCode; 
+            cout << "Enter a code to search: "; 
+            cin >> searchCode; 
+            tree.searchNode(searchCode); 
+        } else if (selection == 4) { 
+            
+        } else if (selection == 5) { 
+        
+        }
+    }
 
     return 0;
 }    
 
-int menuSelection() { 
-    int selection; 
-    do { 
-    cout << "Select Action" <, endl;
+void menuSelection() { 
+    cout << "Select Action" << endl;
     cout << "1. Insert new code" << endl
          << "2. Delete a code" << endl 
          << "3. Search for code" << endl 
          << "4. Modify a code" << endl 
          << "5. Display Tree in postorder" << endl 
          << "6. Quit" << endl; 
-    cin >> "Enter choice: "; 
-    } while (selection != 6); 
-    return selection; 
 }
